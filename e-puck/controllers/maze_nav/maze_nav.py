d0c9c32d6ff203c78ef30372627e25a1bca439dc
raghav-upadhyay2002 <71996199+raghav-upadhyay2002@users.noreply.github.com>
2026-08-17 19:56:30 -0400

    Refactor maze navigation code to improve structure and readability

--- e-puck/controllers/maze_nav/maze_nav.py
+++ e-puck/controllers/maze_nav/maze_nav.py
@@ -1,23 +1,51 @@
 from controller import Robot
+import numpy as np
+import cv2
 
 
-class MazeNavigator(Robot):
-    def __init__(self):
-        super().__init__()
-        self.timestep = int(self.getBasicTimeStep())
-
-    def __del__(self):
-        pass
-
-    def stepBegin(self, duration):
-        pass
-
-    def stepEnd(self):
-        pass
+robot= Robot()
 
 
-if __name__ == "__main__":
-    robot = MazeNavigator()
-    while robot.step(robot.timestep) != -1:
-        robot.stepBegin(robot.timestep)
-        robot.stepEnd()
+
+timestep= int(robot.getBasicTimeStep())
+
+
+camera=  robot.getDevice('Camera')
+camera.enable(timestep)
+
+
+left_motor= robot.getDevice("left wheel motor")
+left_motor.setPostition(float("inf"))
+left_motor.setVelocity(0.0)
+
+
+
+right_motor= robot.getDevice("right wheek motor")
+right_motor.setPostition(float('inf'))
+right_motor.setVelocity(0.0)
+
+
+print('Camera resolution:', camera.getWidth ,'x' , camera.getHeight)
+
+
+while timestep!=-1:
+    image= camera.getImage()
+    width= camera.getWidth()
+    height=camera.getHeight()
+
+    img= np.frombuffer(image, np.uint8).reshape((height,width,4))
+
+    img_bgr= cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+
+    cv2.imshow("Robot Camera Feed", cv2.resize(img_bgr, 300,300))
+
+    cv2.waitKey(1)
+
+
+    left_motor.setVelocity(2.0)
+    right_motor.setVelocity(2.0)
+
+cv2.destroyAllWindows()    
+    
+
